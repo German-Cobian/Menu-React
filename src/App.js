d1bd87420ea5ad9fb12c2874b089bf1ae3b463bc
German Renato Cobian <germancobian@hotmail.com>
2022-05-26 20:48:26 -0700

    Implement dynamic approach to categories for selection of menu items to be displayed in full

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+console.log(allCategories);
+
+
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
   
   const filterItems = (category) => {
     if(category === 'all') {
@@ -24,7 +28,7 @@ function App() {
           <h2>Our Menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- src/Categories.js
+++ src/Categories.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
-const Categories = ({filterItems}) => {
+const Categories = ({categories, filterItems}) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems('breakfast')}>Breakfast</button>
-      <button className="filter-btn" onClick={() => filterItems('lunch')}>Lunch</button>
-      <button className="filter-btn" onClick={() => filterItems('shakes')}>Shakes</button>
-      <button className="filter-btn" onClick={() => filterItems('all')}>All</button>
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+            >
+              {category}
+            </button>
+          )
+        })}
     </div>
   )
 }
